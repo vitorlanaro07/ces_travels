2e9f253ade995cbae2ec1740f58d00f516f5ec59
Vitor S. Lanaro <vitor.lanaro@hotmail.com>
2024-01-20 19:45:58 -0300

    Adding new pages, updating slideshow and progression

---
+++ js/cities.js
@@ -0,0 +1,2 @@
+document.getElementsByClassName('btns_slideshow')[0].style.top = "40%";
+document.getElementsByClassName('btns_slideshow')[1].style.top = "40%";
--- js/script.js
+++ js/script.js
@@ -9,24 +9,40 @@ showDivs(slideIndex);
 function showDivs(n){
     var i;
     var x = document.getElementsByClassName("slides");
-    var names = document.getElementsByClassName("names");
 
+    //To modify number on display Ex: 1/4 -> 2/4 _3/4 -> 4/4 and repeating
+    document.getElementsByClassName("pic_current")[0].innerHTML=n;
+    document.getElementsByClassName("pic_total")[0].innerHTML=x.length;
 
+    console.log(n, x.length);
+    try {
+        var names = document.getElementsByClassName("names");
+    } catch (error) {
+    }
+    
     if (n > x.length){
+        document.getElementsByClassName("pic_current")[0].innerHTML=1;
         slideIndex = 1;
     }
-
     if (n < 1){
+        document.getElementsByClassName("pic_current")[0].innerHTML= x.length;
         slideIndex = x.length;
     }
 
     for (i = 0; i < x.length; i++){
         x[i].style.display = "none";
-        names[i].style.display = "none";
+        try {
+            names[i].style.display = "none";
+        } catch (error) {
+        }
+        
 
     }
     x[slideIndex-1].style.display = "block";
-    names[slideIndex-1].style.display = "block";
+    try {
+        names[slideIndex-1].style.display = "block";
+    } catch (error) {
+    }  
 }
 
 
